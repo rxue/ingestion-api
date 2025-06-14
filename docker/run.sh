7ebaca9423bd77f16a8f828ccd515aeda666415e
Rui Xue <rui.xue@fasolutions.com>
2025-06-14 21:26:41 +0300

    #6 used docker compose instead of docker-compose

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 mvn -f ../java/async-api-endpoint/pom.xml package -Dquarkus.package.jar.type=legacy-jar
 if [ $? -ne 0 ]; then return 1; fi
-docker-compose up async-api-endpoint
+docker compose up async-api-endpoint
 
