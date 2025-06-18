a20de1e98001a7802823903f85b236589874f63d
Rui Xue <rui.xue@fasolutions.com>
2025-06-18 22:55:59 +0300

    #30 refactored StatusLogger

--- java/ingestion-manager/src/main/java/io/github/rxue/ingestion/log/StatusLogger.java
+++ java/ingestion-manager/src/main/java/io/github/rxue/ingestion/log/StatusLogger.java
@@ -26,19 +26,19 @@ public class StatusLogger {
     @AroundInvoke
     public Object log(InvocationContext context) {
         Object target = context.getTarget();
-        try {
-            State state = getState();
-            if (target instanceof StateDescriber stateDescriber) {
-                state.setDescription(stateDescriber.description());
+        if (target instanceof StateDescriber stateDescriber) {
+            try {
                 userTransaction.begin();
+                State state = getState();
+                state.setDescription(stateDescriber.description());
                 entityManager.merge(state);
                 userTransaction.commit();
                 System.out.println("entityManager is " + entityManager);
+            } catch (SystemException | NotSupportedException
+                     | HeuristicRollbackException | HeuristicMixedException
+                     | RollbackException e) {
+                throw new RuntimeException(e);
             }
-        } catch (SystemException | NotSupportedException
-                 | HeuristicRollbackException | HeuristicMixedException
-                | RollbackException e) {
-            throw new RuntimeException(e);
         }
         System.out.println("INTERCEPTED");
 
@@ -49,18 +49,15 @@ public class StatusLogger {
         }
     }
 
-    private State getState() throws SystemException, NotSupportedException,
-            HeuristicRollbackException, HeuristicMixedException, RollbackException {
+    private State getState() {
         System.out.println("going to get state!!!!!");
-        userTransaction.begin();
         List<State> stateList = entityManager.createQuery("select s from State s", State.class)
                 .getResultList();
-        userTransaction.commit();
         System.out.println("state result " + stateList);
         if (stateList.isEmpty()) {
-                return new State();
+            return new State();
         } else {
-                return stateList.get(0);
+            return stateList.get(0);
         }
     }
 
