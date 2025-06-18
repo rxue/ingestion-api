61ac80cb5231ebe03923f0fab9160ab2eff76164
Rui Xue <rui.xue@fasolutions.com>
2025-06-18 22:50:33 +0300

    #30 all database operations need to use UserTransaction for transaction boundary

--- java/ingestion-manager/src/main/java/io/github/rxue/ingestion/log/StatusLogger.java
+++ java/ingestion-manager/src/main/java/io/github/rxue/ingestion/log/StatusLogger.java
@@ -10,7 +10,7 @@ import jakarta.interceptor.Interceptor;
 import jakarta.interceptor.InvocationContext;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.UserTransaction;
+import jakarta.transaction.*;
 
 import java.util.List;
 
@@ -24,37 +24,44 @@ public class StatusLogger {
     private UserTransaction userTransaction;
 
     @AroundInvoke
-    public Object log(InvocationContext context) throws Exception {
+    public Object log(InvocationContext context) {
         Object target = context.getTarget();
-        State state = getState();
+        try {
+            State state = getState();
+            if (target instanceof StateDescriber stateDescriber) {
+                state.setDescription(stateDescriber.description());
+                userTransaction.begin();
+                entityManager.merge(state);
+                userTransaction.commit();
+                System.out.println("entityManager is " + entityManager);
+            }
+        } catch (SystemException | NotSupportedException
+                 | HeuristicRollbackException | HeuristicMixedException
+                | RollbackException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println("INTERCEPTED");
-        //if (target instanceof StateDescriber stateDescriber) {
-          //  state.setDescription(stateDescriber.description());
-          //  System.out.println("is statedescriber");
-            //userTransaction.begin();
-            //entityManager.merge(state);
-            //userTransaction.commit();
-            //System.out.println("entityManager is " + entityManager);
-        //}
-        return context.proceed();
+
+        try {
+            return context.proceed();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private State getState() {
+    private State getState() throws SystemException, NotSupportedException,
+            HeuristicRollbackException, HeuristicMixedException, RollbackException {
         System.out.println("going to get state!!!!!");
-        try {
-            List<State> stateList = entityManager.createQuery("select s from State s", State.class)
-                    .getResultList();
-            System.out.println("state result " + stateList);
-            if (stateList.isEmpty()) {
+        userTransaction.begin();
+        List<State> stateList = entityManager.createQuery("select s from State s", State.class)
+                .getResultList();
+        userTransaction.commit();
+        System.out.println("state result " + stateList);
+        if (stateList.isEmpty()) {
                 return new State();
-            } else {
+        } else {
                 return stateList.get(0);
-            }
-        } catch (Throwable e) {
-            System.out.println("got example" + e);
         }
-        throw new IllegalStateException();
-
     }
 
 }
