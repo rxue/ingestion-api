362303ca1a5af41b58ff9af9362a5205edbbb885
Rui Xue <rui.xue@fasolutions.com>
2025-06-22 13:06:05 +0300

    #43 JobRepository ddl script added to ingestion-manager project

--- docker/functions.sh
+++ docker/functions.sh
@@ -1,14 +1,6 @@
 restartDatabase() {
   docker compose down --volumes database 
   docker compose up -d --build database
-  downloadURL=https://raw.githubusercontent.com/jberet/jsr352/refs/heads/main/jberet-core/src/main/resources/sql/jberet-postgresql.ddl
-  ddlFileName=`basename $downloadURL`
-  echo "Download ddl script ${ddlFileName} for generating status tracking tables used by quarkus-jberet JobRepository"
-  curl -O $downloadURL
-  sed -i '' 's/!!/;/g' $ddlFileName
-  sleep 2
-  echo "Run the ddl script"
-  docker exec -i postgres psql -U postgres -d postgres < $ddlFileName
 }
 restartJMS() {
   docker compose down --volumes broker
@@ -23,11 +15,32 @@ restartAsyncAPIEndpoint() {
   docker compose up -d --build async-api-endpoint
 }
 restartIngestionManager() {
-  docker compose down --volumes ingestion-manager 
-  mvn -f ../java/ingestion-manager/pom.xml clean package -Dquarkus.package.jar.type=legacy-jar
+  docker compose down --volumes ingestion-manager
+  configIngestionManagerJobRepository
+  mvn -f ${projectDir}/pom.xml clean package -Dquarkus.package.jar.type=legacy-jar
   if [ $? -ne 0 ]; then
     echo "Project Build Failure :("
     return 1
   fi
   docker compose up -d --build ingestion-manager
+}
+configIngestionManagerJobRepository() {
+  local downloadURL=https://raw.githubusercontent.com/jberet/jsr352/refs/heads/main/jberet-core/src/main/resources/sql/jberet-postgresql.ddl
+  local ddlFileName=`basename $downloadURL`
+  if [ ! -e "${ddlFileName}" ]; then
+    curl -O "$downloadURL"
+    echo "Download $downloadURL completed"
+  fi
+  if [ ! -e "${ddlFileName}" ]; then
+    echo "file does not exist yet!"
+    return
+  fi
+  projectDir=../java/ingestion-manager
+  projectResourcesDir=${projectDir}/src/main/resources
+  mv $ddlFileName ${projectResourcesDir}
+  quarkusDDLProperty=quarkus.jberet.repository.jdbc.ddl-file
+  configFilePath=${projectResourcesDir}/application.properties
+  if ! grep "$quarkusDDLProperty" ${configFilePath}; then
+    echo "${quarkusDDLProperty}=${ddlFileName}" >> ${configFilePath}
+  fi
 }
