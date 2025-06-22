9ff096a867f632a16f1daa5baa040253cb36b31e
Rui Xue <rui.xue@fasolutions.com>
2025-06-22 20:58:03 +0300

    #48 continued improvement on the output

--- java/ingestion-manager/src/main/java/io/github/rxue/ingestion/SenderStatistic.java
+++ java/ingestion-manager/src/main/java/io/github/rxue/ingestion/SenderStatistic.java
@@ -3,6 +3,6 @@ package io.github.rxue.ingestion;
 public record SenderStatistic(String email, long sentMails) {
     @Override
     public String toString() {
-        return email + " sent " + sentMails;
+        return email + " sent " + sentMails + " mails";
     }
 }
